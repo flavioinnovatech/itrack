9d1ae548f728a138d6580a83239fefa08b60439e
Leandro Guilherme <leandro.guilherme.true@gmail.com>
2011-05-09 17:51:57 -0300

    fala ai pa noi

--- themes/views.py
+++ themes/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render_to_response
 from itrack.system.models import System,Settings
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import render_to_response
 
+@login_required
+@user_passes_test(lambda u: u.groups.filter(name='administradores').count() != 0)
 def index(request):
 	user_system = System.objects.filter(users__username__exact=request.user.username)
-	#system_settings = user_system.
-	return render_to_response("templates/themes.html")
+	
+	for item in user_system:
+	    temp = item.id
+	
+	user_settings = Settings.objects.filter(system=temp)
+
+
+	return render_to_response("templates/themes.html",{ 'user' : request.user, 'system':user_system, 'settings':temp})
--- urls.py
+++ urls.py
@@ -14,16 +14,16 @@ urlpatterns = patterns('',
     url(r'^media/(.*)$', 'django.views.static.serve', {'document_root' : settings.MEDIA_ROOT}),
 	url(r'^accounts/profile/$', 'main.views.index'),
     url(r'^themes/$', 'themes.views.index'),
-<<<<<<< HEAD
+
 	url(r'^grid/$', 'grid.views.index'),
 	url(r'^system/$','system.views.index'),
 	url(r'^rastreamento/veicular$', 'rastreamento.views.index'),
 	url(r'^rastreamento/portatil$', 'rastreamento.views.index'),
 		
 		
-=======
+
 		url(r'^grid/$', 'grid.views.index'),
 		url(r'^system/$','system.views.index'),
 		url(r'^rastreamento/veicular$', 'rastreamento.views.index'),		
->>>>>>> 44ee7bd0faeff0ddb64dfc009dbc195bc80f5d01
+
 )
