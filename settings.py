59c81dd0475e5eac8f6a62c7b164ca8ea6d55266
fabio <fabiothiroki@gmail.com>
2011-05-09 05:31:15 -0300

    to com fome

--- settings.py
+++ settings.py
@@ -48,18 +48,18 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_ROOT_PATH, 'static', 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT_PATH, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -108,7 +108,7 @@ ROOT_URLCONF = 'itrack.urls'
 
 TEMPLATE_DIRS = (
 		PROJECT_ROOT_PATH,
-    "/home/leandro/infotrack-git/itrack/",
+  #  "/home/leandro/infotrack-git/itrack/",
     
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.contrib.auth.views import login,logout
+from django.contrib.auth.views import login,logout,logout_then_login
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,6 +9,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'main.views.index'),
     url(r'^accounts/login/$', login , { 'template_name' : 'accounts/templates/login.html' } ),
-    url(r'^accounts/logout/$', logout  ),
+    url(r'^accounts/logout/$', logout_then_login),
     url(r'^admin/', include(admin.site.urls)),
+		url(r'^media/(.*)$', 'django.views.static.serve', {'document_root' : settings.MEDIA_ROOT}),
+		url(r'^accounts/profile/$', 'main.views.index'),
+    
 )
