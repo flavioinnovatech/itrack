6161bc42374068dcfc86195b197484d52305473a
Leandro Guilherme <leandro.guilherme.true@gmail.com>
2011-05-09 09:15:57 -0300

    Expansão do app system

--- system/views.py
+++ system/views.py
@@ -1,1 +1,22 @@
-# Create your views here.
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import render_to_response
+from itrack.system.models import System
+
+@login_required
+@user_passes_test(lambda u: u.groups.filter(name='administradores').count() != 0)
+def index(request):
+    user_system = System.objects.filter(users__username__exact=request.user.username)
+    
+    return render_to_response("system/templates/home.html",{ 'user' : request.user, 'system':user_system})
+    
+@login_required
+@user_passes_test(lambda u: u.groups.filter(name='administradores').count() != 0)
+def create_form(request):
+    #TO-DO: criar o form
+    return render_to_response("system/templates/create_form.html",{ 'user' : request.user, 'system':user_system})
+    
+@login_required
+@user_passes_test(lambda u: u.groups.filter(name='administradores').count() != 0)
+def create(request):
+    #TO-DO: pegar o request.POST dos dados do sistema, criar um objeto e salvar no banco de dados
+    return render_to_response("system/templates/create_form.html",{ 'user' : request.user, 'system':user_system})
--- urls.py
+++ urls.py
@@ -15,5 +15,6 @@ urlpatterns = patterns('',
 		url(r'^accounts/profile/$', 'main.views.index'),
     url(r'^themes/$', 'themes.views.index'),
 		url(r'^grid/$', 'grid.views.index'),
+		url(r'^system/$','system.views.index'),
 		
 )
