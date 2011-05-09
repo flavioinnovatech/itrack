2c9006e84611b737117016ebd780666184e4d1d5
Leandro Guilherme <leandro.guilherme.true@gmail.com>
2011-05-09 07:47:31 -0300

    Alteração dos atributos da classe settings

--- system/models.py
+++ system/models.py
@@ -23,22 +23,57 @@ class System(Site):
 class Settings(models.Model):
     
     system = models.ForeignKey(System)
+    system.default = 1
     
     title = models.CharField(max_length=200)
     logo = models.ImageField(upload_to='static/img/')
     
-    headerbgcolor = models.CharField(max_length=200)
-    headerborder = models.CharField(max_length=200)
-    headertext = models.CharField(max_length=200)
-   
-    contentbgcolor = models.CharField(max_length=200)
-    contentborder = models.CharField(max_length=200)
-    contenttext = models.CharField(max_length=200)
-   
-    clickbgcolor = models.CharField(max_length=200)
-    clickborder = models.CharField(max_length=200)
-    clicktext = models.CharField(max_length=200)
-       
+    color_site_background = models.CharField(max_length=50)
+    color_table_background = models.CharField(max_length=50)
+    
+    color_menu_gradient_final = models.CharField(max_length=50) 
+    color_menu_gradient_inicial = models.CharField(max_length=50)
+    color_menu_gradient_final_hover = models.CharField(max_length=50)
+    color_menu_gradient_inicial_hover = models.CharField(max_length=50)
+    
+    color_submenu_gradient_final = models.CharField(max_length=50) 
+    color_submenu_gradient_inicial = models.CharField(max_length=50)
+    color_submenu_hover = models.CharField(max_length=50)
+    
+    color_menu_font = models.CharField(max_length=50) 
+    color_menu_font_hover = models.CharField(max_length=50)
+    color_submenu_font = models.CharField(max_length=50)
+    color_submenu_font_hover = models.CharField(max_length=50)
+    
+    color_table_line_hover = models.CharField(max_length=50)
+    color_table_line_font_hover = models.CharField(max_length=50)
+    color_table_header = models.CharField(max_length=50)
+    color_site_font = models.CharField(max_length=50)
+    color_link = models.CharField(max_length=50)
+
+    color_site_background.default = "#e0e0e0"
+    color_table_background.default = "#ffffff"
+    
+    color_menu_gradient_final.default = "#7a7a7a"
+    color_menu_gradient_inicial.default = "#a9a9a9"
+    color_menu_gradient_final_hover.default = "#a1a1a1"
+    color_menu_gradient_inicial_hover.default = "#ebebeb"
+    
+    color_submenu_gradient_final.default = "#cfcfcf"
+    color_submenu_gradient_inicial.default = "#ffffff"
+    color_submenu_hover.default = "#a0a0a0"
+    
+    color_menu_font.default = "#e7e5e5"
+    color_menu_font_hover.default = "#444444"
+    color_submenu_font.default = "#444444"
+    color_submenu_font_hover.default = "#e7e5e5"
+    
+    color_table_line_hover.default = "#dadada"
+    color_table_line_font_hover.default = "#212121"
+    color_table_header.default = "#cccccc"
+    color_site_font.default = "#000000"
+    color_link.default = "#333333"
+
     map_google = models.BooleanField()
     map_multspectral = models.BooleanField()
     map_maplink = models.BooleanField()
