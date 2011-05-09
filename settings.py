89765e96679b8b79b7ae10e77624529db1012203
fabio <fabiothiroki@gmail.com>
2011-05-09 06:50:47 -0300

    pull

--- settings.py
+++ settings.py
@@ -133,6 +133,7 @@ INSTALLED_APPS = (
     'accounts',
     'system',
     'signals',
+		'themes',
 )
 
 # A sample logging configuration. The only tangible logging
--- urls.py
+++ urls.py
@@ -13,5 +13,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 		url(r'^media/(.*)$', 'django.views.static.serve', {'document_root' : settings.MEDIA_ROOT}),
 		url(r'^accounts/profile/$', 'main.views.index'),
-    
+    url(r'^themes/$', 'themes.views.index'),
+		
 )
