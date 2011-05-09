d2babdc540e9b23084a30b14dfb5ccfdff4f0136
Leandro Guilherme <leandro.guilherme.true@gmail.com>
2011-05-09 03:37:10 -0300

    Home pegando infos do usuario

--- main/views.py
+++ main/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render_to_response
 
 @login_required
 def index(request):
-    return render_to_response('main/templates/home.html')
+    return render_to_response('main/templates/home.html',{ 'user' : request.user})
 
 
