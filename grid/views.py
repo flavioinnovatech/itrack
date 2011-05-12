a97883f3dde9c2bc67a6db3edb1d64f9f342dd8f
fabio <fabiothiroki@gmail.com>
2011-05-12 16:35:30 -0300

    config grid

--- grid/views.py
+++ grid/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render_to_response
 from itrack.system.models import System
 from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
-	return render_to_response("templates/configgrid.html")
+	system = System.objects.filter(users__username__exact=request.user.username)
+  
+	return render_to_response("templates/configgrid.html",locals())
