1fcc649b7c678840ee622dd111bc6195c1f0a3b3
Leandro Guilherme <leandro.guilherme.true@gmail.com>
2011-05-12 15:33:40 -0300

    separacao dos forms do system no arquivo forms.py

--- system/views.py
+++ system/views.py
@@ -5,38 +5,8 @@ from django.forms import ModelForm, TextInput
 from django.forms.models import modelform_factory
 from django.http import HttpResponseRedirect
 from django.template.context import RequestContext
+from itrack.system.forms import SystemForm, SettingsForm
 
-class SystemForm(ModelForm):
-	    class Meta:
-	        model = System
-	        exclude = ('parent')
-	        
-
-
-class SettingsForm(ModelForm):
-    class Meta:
-            model = Settings
-            exclude = ('title','system')
-            widgets = {
-                'color_site_background' : TextInput(attrs={'class':'color'}),
-                'color_table_background' : TextInput(attrs={'class':'color'}),
-                'color_menu_gradient_final' : TextInput(attrs={'class':'color'}),
-                'color_menu_gradient_inicial' : TextInput(attrs={'class':'color'}),
-                'color_menu_gradient_final_hover' : TextInput(attrs={'class':'color'}),
-                'color_menu_gradient_inicial_hover': TextInput(attrs={'class':'color'}),
-                'color_submenu_gradient_final': TextInput(attrs={'class':'color'}),
-                'color_submenu_gradient_inicial': TextInput(attrs={'class':'color'}),
-                'color_submenu_hover': TextInput(attrs={'class':'color'}),
-                'color_menu_font': TextInput(attrs={'class':'color'}),
-                'color_menu_font_hover': TextInput(attrs={'class':'color'}),
-                'color_submenu_font': TextInput(attrs={'class':'color'}),
-                'color_submenu_font_hover': TextInput(attrs={'class':'color'}),
-                'color_table_line_hover': TextInput(attrs={'class':'color'}),
-                'color_table_line_font_hover': TextInput(attrs={'class':'color'}),
-                'color_table_header': TextInput(attrs={'class':'color'}),
-                'color_site_font': TextInput(attrs={'class':'color'}),
-                'color_link': TextInput(attrs={'class':'color'}),
-            }
 
 def findChild(parent):
 	vector = []
