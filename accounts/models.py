046475248e460cf5004d1130915bba951990dc1a
Leandro Guilherme <leandro.guilherme.true@gmail.com>
2011-05-15 17:58:48 -0300

    evolução correta

--- accounts/models.py
+++ accounts/models.py
@@ -3,13 +3,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-    user = models.ForeignKey(User,unique=True)
+    profile = models.ForeignKey(User,unique=True)
     telephone = models.CharField(max_length=20)
     cellphone = models.CharField(max_length=20)
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=50)
-    
-    user.default = 1
-    
+
     def __unicode__(self):
       return self.user.username
+      
+    profile.default = 1
