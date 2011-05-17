bee8f18fa2981a374c8ebac8b4a9b64e04262a45
fabio <fabiothiroki@gmail.com>
2011-05-17 15:33:39 -0300

    commit da mais infinita gloria eterna

--- accounts/ajax.py
+++ accounts/ajax.py
@@ -1,12 +1,31 @@
 from django.http import HttpResponse
 from django.utils import simplejson
-
+from itrack.accounts.models import UserProfile
+from django.contrib.auth.models import User
 
 def delete(request):
   
-  post = request.POST
-  print post.id
-  name = request.POST.get('id', False)
+  post = request.POST.copy()
+  i = 0
   
+  while(post.has_key("usuarios["+str(i)+"][id]")):
+    ids = post["usuarios["+str(i)+"][id]"]
+    print ids
+    
+    user = User.objects.get(pk=ids)
+    profile = UserProfile.objects.get(profile=ids)
+    
+    # Usuario nao pode se auto deletar
+    system = request.session['system']    
+    users = User.objects.filter(system=system)
+    print users
+    if (user in users):
+      return HttpResponse('falhou')
+           
+    else:
+      user.delete()
+      profile.delete()
+           
+    i = i+1
   
-  return HttpResponse(name)
+  return HttpResponse('sucesso')
