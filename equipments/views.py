1d5532f0faba3c1721b9defe833439b691c22672
fabio <fabiothiroki@gmail.com>
2011-05-26 09:56:37 -0300

    push

--- equipments/views.py
+++ equipments/views.py
@@ -52,7 +52,7 @@ def index(request):
     for item in childs:
         system = System.objects.get(pk=item)
         print system.__dict__
-        rendered_list+=u"<tr style='width:5%;' ><td style='width:50%;'>"+system.name+": </td><td style='text-align:center;'><a class='table-button' href=\"/equipment/permissions/edit/"+str(system.id)+"/\">Editar</a>  <a class='table-button' href=\"/equipment/permissions/delete/"+str(system.id)+"/\">Apagar</a></td></tr>"
+        rendered_list+=u"<tr style='width:5%;' ><td style='width:50%;'>"+system.name+": </td><td style='text-align:center;'><a class='table-button' href=\"/equipment/permissions/"+str(system.id)+"/\">Editar</a>  <a class='table-button' href=\"/equipment/associations/"+str(system.id)+"/\">Equipamentos</a></td></tr>"
         vector_html.append({'name':System.objects.get(pk=item).name, 'id': item})
     
     return render_to_response("equipments/templates/home.html",locals())
